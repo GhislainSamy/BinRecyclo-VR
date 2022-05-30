4c78c6d5707a9acfc2f1a1df472a401607fc9489
GhislainSamy <45396212+GhislainSamy@users.noreply.github.com>
2022-05-30 21:25:27 +0200

    Integration OVR
    Création du menu de jeu en VR,
    
    integration OVR pour le niveau 1

---
+++ Assets/MesAssets/Scripts/MenuController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+   public void niveau1(){
+       SceneManager.LoadScene("Niveau_1");
+   }
+}
